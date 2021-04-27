555a8f67eb2200aa1a70262a96187353c80f2aa4
Cristian Ogliari <cogliari@gmail.com>
2021-04-27 19:19:33 -0300

    first commit

---
+++ backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/trainee_devinhouse/DevTraineeSoftplanPsApplication.java
@@ -0,0 +1,13 @@
+package br.com.devinhouse.trainee.trainee_devinhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTraineeSoftplanPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTraineeSoftplanPsApplication.class, args);
+	}
+
+}
---
+++ backend/trainee_devinhouse/src/test/java/br/com/devinhouse/trainee/trainee_devinhouse/DevTraineeSoftplanPsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devinhouse.trainee.trainee_devinhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTraineeSoftplanPsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
