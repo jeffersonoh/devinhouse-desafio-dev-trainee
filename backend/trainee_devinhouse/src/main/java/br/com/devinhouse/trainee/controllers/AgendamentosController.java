a06d32157880bb61beb33d03262dd75e059c3b18
Cristian Ogliari <cogliari@gmail.com>
2021-04-28 23:07:04 -0300

    create: agendamentos controller

--- backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/controllers/AgendamentosController.java
+++ backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/controllers/AgendamentosController.java
@@ -1,6 +1,17 @@
 package br.com.devinhouse.trainee.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.devinhouse.trainee.services.AgendamentoServices;
+
+@RestController
+@RequestMapping(value = "/agendamentos/v1", headers = "api-version=1")
 public class AgendamentosController {
+	
+	@Autowired
+	private AgendamentoServices agendamentoServices;
 
 	// Deverá haver um endpoint para inclusao de um agendamento
 	// Deverá haver um endpoint para edição de um agendamento realizado, apenas dia e hora poderão ser editados;
