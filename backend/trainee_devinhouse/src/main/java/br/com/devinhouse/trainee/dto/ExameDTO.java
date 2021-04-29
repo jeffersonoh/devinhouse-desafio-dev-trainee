70a5e037837634654af703c3c442537a74e235f6
Cristian Ogliari <cogliari@gmail.com>
2021-04-29 20:20:36 -0300

    correction: clean exame dto

--- backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/dto/ExameDTO.java
+++ backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/dto/ExameDTO.java
@@ -4,30 +4,4 @@ import br.com.devinhouse.trainee.entities.Exame;
 
 public class ExameDTO {
 
-	private int id;
-	private String nome;
-
-	public ExameDTO() {
-		
-	}
-	
-	public ExameDTO(Exame exame) {
-		this.nome = exame.getNome();
-	}	
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 }
