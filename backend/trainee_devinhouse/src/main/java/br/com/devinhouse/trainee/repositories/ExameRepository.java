0da1d31abf773f3c65c0929a521ed27687499106
Cristian Ogliari <cogliari@gmail.com>
2021-04-29 20:47:26 -0300

    correction: add repository annotation

--- backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/repositories/ExameRepository.java
+++ backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/repositories/ExameRepository.java
@@ -1,9 +1,11 @@
 package br.com.devinhouse.trainee.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.devinhouse.trainee.entities.Exame;
 
+@Repository
 public interface ExameRepository extends JpaRepository<Exame, Integer> {
 
 }
