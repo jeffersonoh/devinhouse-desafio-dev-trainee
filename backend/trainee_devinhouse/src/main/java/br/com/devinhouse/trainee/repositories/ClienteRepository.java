7f163b7f99f6beb9958a3cf384fd4b1eeab2a4be
Cristian Ogliari <cogliari@gmail.com>
2021-04-29 20:47:08 -0300

    correction: add repository annotation

--- backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/repositories/ClienteRepository.java
+++ backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/repositories/ClienteRepository.java
@@ -1,9 +1,11 @@
 package br.com.devinhouse.trainee.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.devinhouse.trainee.entities.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
 }
