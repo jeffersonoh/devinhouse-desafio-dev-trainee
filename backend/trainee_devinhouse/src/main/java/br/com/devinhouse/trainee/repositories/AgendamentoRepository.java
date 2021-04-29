c1c48a64284507b23199e95a5ba09a90f4123b8a
Cristian Ogliari <cogliari@gmail.com>
2021-04-29 20:47:41 -0300

    correction: add repository annotation

--- backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/repositories/AgendamentoRepository.java
+++ backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/repositories/AgendamentoRepository.java
@@ -2,9 +2,11 @@ package br.com.devinhouse.trainee.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.devinhouse.trainee.entities.Agendamento;
 
+@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer>{
 
 }
