fa0ddd16ea6a9b623266a7321ab7157be12df6e4
Cristian Ogliari <cogliari@gmail.com>
2021-04-28 23:07:26 -0300

    finish: clientes controller

--- backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/controllers/ClienteController.java
+++ backend/trainee_devinhouse/src/main/java/br/com/devinhouse/trainee/controllers/ClienteController.java
@@ -1,11 +1,63 @@
 package br.com.devinhouse.trainee.controllers;
 
-public class ClienteController {
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-	// Deverá haver um endpoint para criação de um cliente;
-	// Deverá haver um endpoint para atualização de um cliente;
-	// Deverá haver um endpoint para busca de um cliente baseado no seu cpf;
-	// Deverá haver um endpoint para listagem de todos os clientes cadastrados;
-	// Deverá haver um endpoint para exclusão de um cliente;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.devinhouse.trainee.entities.Cliente;
+import br.com.devinhouse.trainee.services.ClienteServices;
+
+@RestController
+@RequestMapping(value = "/clientes/v1", headers = "api-version=1")
+public class ClienteController {
 	
+	@Autowired
+	private ClienteServices clienteServices;
+	
+	// Deverá haver um endpoint para criação de um cliente;
+	@RequestMapping(value = "/cadastrar", method = POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE )
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente registerClient(@RequestBody Cliente obj) {
+		return clienteServices.create(obj);
+	}
+	
+	// Deverá haver um endpoint para listagem de todos os clientes cadastrados;
+	@RequestMapping(value = "/consultar", method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public List<Cliente> getAllClients() {
+		return clienteServices.getAll();
+	}
+	
+	// Deverá haver um endpoint para busca de um cliente baseado no seu cpf;
+	@RequestMapping(value = "/consultar/cpf/{cpf}", method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public Cliente getByCPF(@PathVariable Long cpf) {
+		return clienteServices.getByCPFKey(cpf);
+	}
+		
+	// Deverá haver um endpoint para atualização de um cliente;
+	
+	@RequestMapping(value = "/atualizar/cpf/{cpf}", method = PUT, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)	
+	public Cliente updateClient(@PathVariable Long cpf, @RequestBody Cliente obj) {
+		return clienteServices.updateClientByCPF(cpf, obj);
+	}
+		
+	// Deverá haver um endpoint para exclusão de um cliente;
+	@RequestMapping(value = "/deletar/cpf/{cpf}", method = DELETE, produces = APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public List<Cliente> removeClient(@PathVariable Long cpf) {
+		return clienteServices.delete(cpf);
+	}
 }
