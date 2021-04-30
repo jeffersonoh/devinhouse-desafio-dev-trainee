f81279950a3953a1a50bb59307dd4532ae03a0a8
Mauricio Sampaio <mausampaio@gmail.com>
2021-04-30 11:21:16 -0300

    feat: cria componente listitemlink

---
+++ frontend/src/components/ListItemLink.js
@@ -0,0 +1,22 @@
+import { useMemo, forwardRef } from "react";
+import { Link } from "react-router-dom";
+
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+export default function ListItemLink({ icon, primary, to }) {
+  const CustomLink = useMemo(
+    () => forwardRef((props, ref) => <Link ref={ref} to={to} {...props} />),
+    [to]
+  );
+
+  return (
+    <li>
+      <ListItem button component={CustomLink}>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
+}
