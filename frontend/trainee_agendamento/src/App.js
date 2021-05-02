20996d5ac0871587c8a7a5d186345e4246a1d779
Cristian Ogliari <cogliari@gmail.com>
2021-05-02 02:13:08 -0300

    finish: configuration files add packages and corrections on app.js

--- frontend/trainee_agendamento/src/App.js
+++ frontend/trainee_agendamento/src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { routes } from "./routes/routes";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
-return (
-  <>
-    <ToastContainer />
-    <h1>Hello World</h1>
-  </>
-)
+  return (
+    <BrowserRouter>
+      <ToastContainer />
+      {routes.map((route, index) => (
+        <Route 
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          component={route.component}
+        />
+      ))}
+      <GlobalStyles />
+    </BrowserRouter>      
+  )
 }
 
 export default App;
