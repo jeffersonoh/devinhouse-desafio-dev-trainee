51cf39b3c30ad79ae804d246af98982135616baa
gustavoluchi <gustavo.luchi@gmail.com>
2021-05-02 15:31:16 -0300

    Deixando as importações mais limpas

--- backend/src/main/java/br/com/devtrainee/desafiodev/model/Exam.java
+++ backend/src/main/java/br/com/devtrainee/desafiodev/model/Exam.java
@@ -1,20 +1,8 @@
 package br.com.devtrainee.desafiodev.model;
 
-import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Builder;
 
