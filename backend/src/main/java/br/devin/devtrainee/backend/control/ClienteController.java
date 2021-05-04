91a11c7da3ce887bde7461c945a2662c2bb6cd57
GuiNoronhaS <guinoronha7@gmail.com>
2021-05-04 00:46:57 -0300

    Cors

--- backend/src/main/java/br/devin/devtrainee/backend/control/ClienteController.java
+++ backend/src/main/java/br/devin/devtrainee/backend/control/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import br.devin.devtrainee.backend.service.ClienteService;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ClienteController {
 	
 	@Autowired
--- backend/src/main/java/br/devin/devtrainee/backend/control/ExameController.java
+++ backend/src/main/java/br/devin/devtrainee/backend/control/ExameController.java
@@ -3,6 +3,7 @@ package br.devin.devtrainee.backend.control;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import br.devin.devtrainee.backend.model.Exame;
 import br.devin.devtrainee.backend.service.ExameService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ExameController {
 	
 	@Autowired
