fa63a09cca2b094856d6b48adcab171a707685e9
jorj <jorj@whirlpool>
2021-05-06 08:03:49 -0300

    chore: utils

--- src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_BASE = "http://localhost:8080/v1";
-
-export const getExames = () => {
-    return axios.get(API_BASE + "/exames")
-}
---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:8080/v1";
+
+export const getExames = () => axios.get(API_BASE + "/exames");
+
+export const getClientes = () => axios.get(API_BASE + "/clientes");
+
+export const searchClientesByCpf = (cpf) => axios.get(API_BASE + "/clientes?cpf=" + cpf);
+
+export const deleteCliente = (id) => axios.delete(API_BASE + "/clientes/" + id);
+
+export const createCliente = (data) => axios.post(API_BASE + "/clientes", data);
+
+export const updateCliente = (data) => axios.put(API_BASE + "/clientes/" + data.id, data);
+
+export const patchAgendamento = (data) => axios.patch(API_BASE + "/agendamentos/" + data.id, data);
