40b355525091d4a27cd27f74c8a9cbbb448e2db8
GuiNoronhaS <guinoronha7@gmail.com>
2021-05-07 02:23:15 -0300

    Removendo imports sem uso

--- backend/src/main/java/br/devin/devtrainee/backend/repository/AgendamentoRepository.java
+++ backend/src/main/java/br/devin/devtrainee/backend/repository/AgendamentoRepository.java
@@ -3,8 +3,6 @@ package br.devin.devtrainee.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.devin.devtrainee.backend.model.Agendamento;
