c7e5455c6ff589c04e81294c95b088487c3d15ea
ejnn <eduardojorgenicolau@gmail.com>
2021-05-07 20:39:22 -0300

    fix: CORS

--- src/main/java/devtrainee/ejnn/backend/BackendApplication.java
+++ src/main/java/devtrainee/ejnn/backend/BackendApplication.java
@@ -5,11 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class BackendApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			};
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
